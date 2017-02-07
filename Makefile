820f99120f5b86f5c7107a8fd0245437d958e127
Nick Zalutskiy <pacemkr@gmail.com>
2017-02-06 19:00:08 -0500

    Sort of compiles.

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ gen-c-stubs: lib/Fuse_bindings.idl
 	camlidl -header $<
 
 include $(shell opam config var solvuu-build:lib)/solvuu.mk
+
+#ocamlc -a    -o ocamlfuse.cma -linkall lib/Result.cmo lib/Fuse_bindings.cmo lib/Thread_pool.cmo lib/Fuse_lib.cmo lib/Unix_util.cmo lib/Fuse.cmo -dllib -lcamlidl -dllib -locamlfuse -cclib -lcamlidl -cclib -locamlfuse
--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -3,11 +3,21 @@ open Solvuu_build.Std
 let project_name = "ocamlfuse"
 let version = "2.8.0" (* version bump? *)
 
+let ocamlfuse_lib = Build.lib
+ ~dir:"lib"
+ ~findlib_deps:["unix"; "threads"; "bigarray"; "camlidl"]
+ ~pkg:project_name
+
+
+
 let ocamlfuse_lib = Project.lib project_name
     ~dir:"lib"
     ~style:`Basic
     ~thread:()
+    ~cclib:"-lcamlidl"
     ~findlib_deps:["unix"; "threads"; "bigarray"; "camlidl"]
+    ~linkall:()
+    ~verbose:()
     ~pkg:project_name
 
 let () = Project.basic1 ~project_name ~version [ocamlfuse_lib]
